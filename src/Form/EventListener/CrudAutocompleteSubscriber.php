e68ed6476616ed6ee0bb074c4729df1e29228e0e
Marcel Berteler <pluk77@gmail.com>
2025-11-05 09:22:18 +0200

    Remove dependancy on id_reader
    Remove the dependancy on id_reader and add conversion to database values for types other than integers and strings

--- src/Form/EventListener/CrudAutocompleteSubscriber.php
+++ src/Form/EventListener/CrudAutocompleteSubscriber.php
@@ -2,15 +2,15 @@
 
 namespace EasyCorp\Bundle\EasyAdminBundle\Form\EventListener;
 
+use Doctrine\DBAL\Types\Type;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
-use Symfony\Component\Uid\Ulid;
-use Symfony\Component\Uid\Uuid;
 
 /**
  * @author Yonel Ceruto <yonelceruto@gmail.com>
+ * @author Marcel Berteler <pluk77@gmail.com>
  */
 class CrudAutocompleteSubscriber implements EventSubscriberInterface
 {
@@ -38,6 +38,8 @@ class CrudAutocompleteSubscriber implements EventSubscriberInterface
     }
 
     /**
+     * This method assumes that the object has a single-column primary key.
+     *
      * @return void
      */
     public function preSubmit(FormEvent $event)
@@ -49,27 +51,13 @@ class CrudAutocompleteSubscriber implements EventSubscriberInterface
         if (!isset($data['autocomplete']) || '' === $data['autocomplete']) {
             $options['choices'] = [];
         } else {
-            if (false === $options['id_reader']->isIntId()) {
-                if (!\is_array($data['autocomplete'])) {
-                    $data['autocomplete'] = [$data['autocomplete']];
-                }
+            $data['autocomplete'] = $this->processAutocompleteValues($data['autocomplete'], $options);
 
-                $data['autocomplete'] = array_map(
-                    function ($v) {
-                        if (Ulid::isValid($v)) {
-                            return Ulid::fromBase32($v)->toRfc4122();
-                        } elseif (Uuid::isValid($v)) {
-                            return Uuid::fromString($v)->toBinary();
-                        }
-
-                        return $v;
-                    },
-                    $data['autocomplete']
-                );
-            }
-
+            // Assume primary key is not a composit key, or take the first key
+            $idFieldName = current($options['em']->getClassMetadata($options['class'])->getIdentifierFieldNames());
+            
             $options['choices'] = $options['em']->getRepository($options['class'])->findBy([
-                $options['id_reader']->getIdField() => $data['autocomplete'],
+                $idFieldName => $data['autocomplete'],
             ]);
         }
 
@@ -77,5 +65,64 @@ class CrudAutocompleteSubscriber implements EventSubscriberInterface
         unset($options['em'], $options['loader'], $options['empty_data'], $options['choice_list'], $options['choices_as_values']);
 
         $form->add('autocomplete', EntityType::class, $options);
+    }
+
+    /**
+     * Some field types require a conversion to the database value before 
+     * it can be used in the findBy query. Examples of these are UUIDs and ULIDs.
+     *
+     * Integers and string types are returned without processing
+     *
+     * If the primary key of a type defined Doctrine then the values are converted
+     * from the entity to the database value.
+     *
+     * Otherwise, the values are returned without change.
+     *
+     * For example, if the used platform is MySQL:
+     *
+     *      App\Entity\Category {#1040 ▼
+     *          -id: Symfony\Component\Uid\UuidV6 {#1046 ▼
+     *              #uid: "1ec4d51f-c746-6f60-b698-634384c1b64c"
+     *          }
+     *          -title: "cat 2"
+     *      }
+     *
+     *  gets processed to a binary value:
+     *
+     *      b"\x1EÄÕ\x1FÇFo`¶˜cC„Á¶L"
+     */
+    private function processAutocompleteValues(mixed $autocompleteValues, array $options): array
+    {
+        if (!\is_array($autocompleteValues)) {
+            $autocompleteValues = [$autocompleteValues];
+        }
+
+        $classMetadata = $options['em']->getClassMetadata($options['class']);
+        $ids = $classMetadata->getIdentifierFieldNames();
+
+        // Assume primary key is not a composit key, or take the first
+        $idType = $classMetadata->getTypeOfField(current($ids));
+
+        // integers and strings do not need processing
+        if (\in_array($idType, ['integer', 'smallint', 'bigint', 'string'], true)) {
+            return $autocompleteValues;
+        }
+
+        try {
+            // all values have the same platform and type
+            $type = Type::getType($idType);
+            $platform = $options['em']->getConnection()->getDatabasePlatform();
+
+            return array_map(
+                function ($v) use ($type, $platform) {
+                    return $type->convertToDatabaseValue($v, $platform);
+                },
+                $autocompleteValues
+            );
+        } catch (\Throwable $exception) {
+            // if the conversion fails we return the unmodified values
+        }
+
+        return $autocompleteValues;
     }
 }
